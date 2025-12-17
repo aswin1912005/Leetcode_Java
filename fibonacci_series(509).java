71cb1840b8afe9a294305c6341c59da5f4080669
aswin <aswinchandrasekar@outlook.com>
2025-12-17 21:28:06 +0530

    Create fibonacci_series(509).java

---
+++ fibonacci_series(509).java
@@ -0,0 +1,18 @@
+class Solution {
+    public int fib(int n) {
+        int f1,f2,f3;
+        f1=0;
+        f2=1;
+        f3=0;
+        if(n==0)
+        return 0;
+        if (n == 1) 
+        return 1;
+        for(int i=2;i<=n;i++){
+            f3=f1+f2;
+            f1=f2;
+            f2=f3;
+        }
+        return f3;
+    }
+}
